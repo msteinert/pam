3v1n0/go-pam

transaction, moduler: Do not export PAM conv handler function to modules
This function is only needed when using go PAM for creating applications
so it's not something we expect to have exported to library modules.

To prevent this use an `asPamModule` tag to prevent compilation of
application-only features.

cmd/pam-moduler/moduler.go
@@ -119,9 +119,9 @@ func main() {
 		tags = strings.Split(*buildTags, ",")
 	}
 
-	var generateTags []string
+	generateTags := []string{"asPamModule"}
 	if len(*moduleBuildTags) > 0 {
-		generateTags = strings.Split(*moduleBuildTags, ",")
+		generateTags = append(generateTags, strings.Split(*moduleBuildTags, ",")...)
 	}
 
 	var buildFlags []string

msteinert/pam

cmd/pam-moduler/moduler.go
@@ -119,9 +119,9 @@ func main() {
 		tags = *buildTags
 	}
 
-	var generateTags []string
+	generateTags := []string{"go_pam_module"}
 	if len(*moduleBuildTags) > 0 {
-		generateTags = strings.Split(*moduleBuildTags, ",")
+		generateTags = append(generateTags, strings.Split(*moduleBuildTags, ",")...)
 	}
 
 	var buildFlags []string